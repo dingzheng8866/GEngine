f12872241308bd8b7a39f2b5ddc079cc55621a55
xiu <xiuxiu@163.com>
2018-03-11 20:16:36 +0800

    init

---
+++ KEngine.UnityProject/Assets/KEngine.Demo/WinTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTest : MonoBehaviour {
+
+    public string tag = "";
+
+    private Button Button1;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(tag+"-->" + Screen.width +":"+Screen.height);
+        Button1 = GetComponent<Button>();
+        //Button1.onClick.AddListener(() => Debug.Log("Click hidden close Button!"+tag));
+
+        Button1.onClick.AddListener(delegate () {
+            this.OnClick();
+        });
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Click hidden close Button!" + tag);
+        if(tag == "rootwin")
+        {
+            Transform tf = transform.Find("wincontainer");
+            if(tf!=null)
+            {
+                if (tf.gameObject.activeSelf)
+                {
+                    tf.gameObject.SetActive(false);
+                }
+                else
+                {
+                    tf.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //if (Input.GetMouseButton(0))
+        //{
+            //Debug.Log(tag+"-------" + Input.mousePosition);
+        //}
+	}
+}
---
+++ KEngine.UnityProject/Assets/KEngine.ResourceDep/ResourceDepShaders.cs
@@ -0,0 +1,12 @@
+﻿#if !(UNITY_5 || UNITY_2017)
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 用于AssetBundle打包Shader
+/// </summary>
+public class ResourceDepShaders : MonoBehaviour
+{
+    public Shader[] Shaders;
+}
+#endif
